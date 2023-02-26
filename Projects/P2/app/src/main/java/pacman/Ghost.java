05765f05efd9abb7b33a2070169e2964b9659e16
Keshav Ganapathy <kganapathy23@gmail.com>
2023-02-26 16:07:12 -0500

    ghost-valid_move changes

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -13,9 +13,39 @@ public class Ghost {
     this.myMap = map;
   }
 
-  public ArrayList<Location> get_valid_moves() {
-    return null;
+  public boolean valid_move(Location loc){
+    int x = loc.x;
+    int y = loc.y;
+    HashSet<Type> moves = this.myMap.getLoc(new Location(x,y));
+    if(moves.contains(Map.Type.WALL)) {
+      return false;
+    }
+    return true;
   }
+
+  public ArrayList<Location> get_valid_moves() {
+    int x = this.myLoc.x;
+    int y = this.myLoc.y;
+    ArrayList<Location> return_val = new ArrayList();
+    return_val.add(new Location(x, y+1));
+    return_val.add(new Location(x+1, y+1));
+    return_val.add(new Location(x-1, y+1));
+    return_val.add(new Location(x+1, y-1));
+    return_val.add(new Location(x-1, y-1));
+    return_val.add(new Location(x, y-1));
+    return_val.add(new Location(x+1, y));
+    return_val.add(new Location(x-1, y));
+    int i = 0;
+    for (Location loc: return_val){
+      if (valid_move(loc) == true) {
+        i++;
+      }else{
+        return_val.remove(i);
+      }
+    }
+    return return_val;
+  }
+
 
   public boolean move() {
     return false;
