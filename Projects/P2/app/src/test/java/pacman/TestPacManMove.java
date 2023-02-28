4830bc5f2b168e7f5865b9d0e650f6168af79684
jwernethUMD <jwerneth@terpmail.umd.edu>
2023-02-27 22:47:50 -0500

    Created tests for pacman move

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -6,6 +6,23 @@ import java.awt.Color;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    return;
+    //Creating A Map
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+    //Creating Players
+    PacMan pacman = frame.addPacMan(new Location(2, 3)); //Creates a red ghost named "name" at location x,y
+    ArrayList<Location> moves = pacman.get_valid_moves();
+
+    assertTrue(pacman.move());
+    Map map = frame.getMap();
+
+    int found = 0;
+    for (Location loc : moves) {
+      if (map.getLoc(loc).contains(Type.PACMAN)) {
+        found++;
+      }
+    }
+
+    assertTrue(found == 1);
   }
 }
