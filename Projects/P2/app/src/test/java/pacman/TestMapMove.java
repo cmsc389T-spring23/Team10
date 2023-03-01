545a4da6b23a5ab0caea7e5558ae03e7b397c2ae
Keshav Ganapathy <kganapathy23@gmail.com>
2023-03-01 16:22:16 -0500

    added my tests

--- Projects/P2/app/src/test/java/pacman/TestMapMove.java
+++ Projects/P2/app/src/test/java/pacman/TestMapMove.java
@@ -2,10 +2,20 @@ package pacman;
 import java.io.*;
 import junit.framework.*;
 import java.awt.Color;
+import java.util.*;
 
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    return;
+    //Creating A Map
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+    //Creating Players
+    Ghost ghost = frame.addGhost(new Location(1, 1), "Blinky", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(1, 2)); //Creates PacMan at location x, y
+
+    Map map = frame.getMap();
+    ArrayList<Location> move = pacman.get_valid_moves();
+    assertTrue(map.move("pacman", move.get(0), Map.Type.PACMAN));
   }
 }
--- Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManValidMoves.java
@@ -6,6 +6,14 @@ import java.awt.Color;
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    return;
+    //Creating A Map
+    NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+    //Creating Players
+    Ghost ghost = frame.addGhost(new Location(1, 1), "Blinky", Color.red); //Creates a red ghost named "name" at location x,y
+    PacMan pacman = frame.addPacMan(new Location(1, 2)); //Creates PacMan at location x, y
+
+    Map map = frame.getMap();
+    assertTrue(pacman.get_valid_moves().size() > 0);
   }
 }
